1aae88fb7e9ed135d319f5081ed43a1321c9083f
Robert Main <lenswipe@gmail.com>
2020-04-25 15:12:03 -0400

    First commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <Home />
+  </div>
+</template>
+
+<script>
+import Home from './pages/Home.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Home,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
