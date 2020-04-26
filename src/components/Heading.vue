b888b11ffa1484a278c80ef6c6d140bc4770e5af
Robert Main <lenswipe@gmail.com>
2020-04-25 21:43:30 -0400

    Add heading component

---
+++ src/components/Heading.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  name: 'Heading',
+  props: {
+    level: {
+      type: [String, Number],
+      required: true,
+      validator: (value) => Number.isInteger(parseInt(value))
+        && parseInt(value) >= 0
+        && parseInt(value) <= 5,
+    }
+  },
+  computed: {
+    computedLevel: function () {
+      return parseInt(this.level);
+    },
+  },
+  render(createElement) {
+    return createElement(`h${this.computedLevel}`, `☝ ${this.$slots.default[0].text}`)
+  }
+}
+</script>
