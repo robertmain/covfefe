df52018ea23420f76f21f47249a9aff5a95d91c2
Robert Main <lenswipe@gmail.com>
2025-02-01 02:23:49 -0500

    Fix lint errors

--- src/components/FlexibleHeading.test.tsx
+++ src/components/FlexibleHeading.test.tsx
@@ -1,24 +1,24 @@
-import { describe, expect, it, vi } from 'vitest'
+import { describe, expect, it, vi, afterEach, beforeEach, type MockInstance } from 'vitest'
 import { cleanup, render } from '@testing-library/vue'
 import FlexibleHeading from './FlexibleHeading.vue'
-import { afterEach } from 'node:test'
 
 describe('FlexibleHeading', () => {
-  it('does not allow negative heading levels', async () => {
-    const spy = vi.spyOn(console, 'warn').mockImplementation(() => {})
-    render(FlexibleHeading, { props: { level: -1 } })
-    expect(spy.mock.calls[0][0]).toMatch(/invalid prop: custom validator/i)
-    spy.mockRestore()
+  let warnMock: MockInstance<Console['warn']>
+  beforeEach(() => {
+    warnMock = vi.spyOn(console, 'warn').mockImplementation(() => {})
+  })
+  afterEach(() => {
+    warnMock.mockRestore()
     cleanup()
+  })
+  it('does not allow negative heading levels', async () => {
+    render(FlexibleHeading, { props: { level: -1 } })
+    expect(warnMock.mock.calls[0][0]).toMatch(/invalid prop: custom validator/i)
   })
   it('does not allow heading levels greater than 5', async () => {
-    const spy = vi.spyOn(console, 'warn').mockImplementation(() => {})
     render(FlexibleHeading, { props: { level: 6 } })
-    expect(spy.mock.calls[0][0]).toMatch(/invalid prop: custom validator/i)
-    spy.mockRestore()
-    cleanup()
+    expect(warnMock.mock.calls[0][0]).toMatch(/invalid prop: custom validator/i)
   })
-
   it('renders the text provided', async () => {
     const resutlt = render(<FlexibleHeading level={1}>Hello!</FlexibleHeading>)
     expect(resutlt.queryByRole('heading')?.textContent).toContain('Hello')
