479fdf26f337e50a008ea450533ab8134befddc8
Robert Main <lenswipe@gmail.com>
2025-02-01 22:58:01 -0500

    Fix lazyloading of component

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-const routes = [{ path: '/', component: import('@/pages/HomePage.vue') }]
+const routes = [
+  {
+    path: '/',
+    component: () => import('@/pages/HomePage.vue'),
+  },
+]
 
 export const router = createRouter({
   history: createWebHistory('covfefe'),
