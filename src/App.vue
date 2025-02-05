d4aa49b8d1d6af1f91d3524fe8b3870e70097968
Robert Main <lenswipe@gmail.com>
2025-02-04 22:39:39 -0500

    Extract base styles
    Extract base scss into external file for easier organization

--- src/App.vue
+++ src/App.vue
@@ -24,32 +24,6 @@ import AppTitle from '@/components/AppTitle.vue'
 @import './assets/fonts/tinyhand/style.css';
 @import url('https://fonts.googleapis.com/css2?family=Montserrat&display=swap');
 
-:root {
-  // Spacing
-  --spacing-xs: 5px;
-  --spacing-sm: 10px;
-  --spacing-md: 20px;
-  --spacing-lg: 40px;
-
-  // Colors
-  --mid-orange: orange;
-  --mid-yellow: yellow;
-
-  //Greys
-  --light-grey-1: #eee;
-  --light-grey-2: #ddd;
-  --light-grey-3: #ccc;
-  --light-grey-4: #a9a9a9;
-
-  --mid-grey-1: #777;
-  --mid-grey-2: #575757;
-  --mid-grey-3: #444;
-
-  --dark-grey-1: #333;
-  --dark-grey-2: #222;
-  --dark-grey-1: #111;
-}
-
 body {
   background: repeating-conic-gradient(
     from 0deg,
